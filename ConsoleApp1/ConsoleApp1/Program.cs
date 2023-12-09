dbdf14cb8844b2dc748797fe0d513cb1d497ab0c
LAKSHAY BANSAL <119696323+LAKSHAYBANSAL879@users.noreply.github.com>
2023-12-09 18:04:52 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  this is important for debigging do NotFiniteNumberException remove comment
+            int lbansal = 99;
+            //data types in c#
+            /*
+             integer int
+            float- float number 
+              character - char a='A';
+            Boolean-bool isGreat=true;
+           String- string inp="Lakshay"
+
+              
+             */
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello my name is lakshay bansal");
+            Console.WriteLine("Hello my name is lakshay bansal and my fav num is"+lbansal);
+           
+
+            Console.ReadLine();
+        }
+    }
+}
